e02f3039af6b4424a18a7c5c51f580dbce1ddcc8
Your Name <you@example.com>
2025-09-18 10:37:49 +0200

    added a register qml types

--- src/qtquick3d_opencv_helpers/__init__.py
+++ src/qtquick3d_opencv_helpers/__init__.py
@@ -1,1 +1,10 @@
-#
+from PySide6.QtQml import qmlRegisterType
+from .backend import SystemMonitor
+
+from .grid_geometry import GridLinesGeometry, AxisLineGeometry
+
+def register_qml_types():
+    """Call this function to register Python types with the QML engine."""
+    # qmlRegisterType(PythonClass, URI, VersionMajor, VersionMinor, QMLName)
+    qmlRegisterType(GridLinesGeometry, "CustomGrid", 0, 1, "GridLinesGeometry")
+    qmlRegisterType(AxisLineGeometry, "CustomGrid", 0, 1, "AxisLineGeometry")
